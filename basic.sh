a15e3493847e49513ec50d57c4b18f38dc2c4572
Miroslav Buček <miroslavbucek@gmail.com>
2018-08-19 15:38:17 +0200

    Update basic.sh
    old debian update

--- basic.sh
+++ basic.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # update
-apt update
+apt-get update
 
 # install basic packages
-apt -y install vim zsh git mc wget htop avahi-daemon
+apt-get -y install vim zsh git mc wget htop avahi-daemon
 
 # timezone Prague
 echo "Europe/Prague" > /etc/timezone 
@@ -12,7 +12,7 @@ dpkg-reconfigure -f noninteractive tzdata
 
 # security updates
 echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
-apt -y install unattended-upgrades
+apt-get -y install unattended-upgrades
 
 # install oh my zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
