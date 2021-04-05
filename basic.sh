685103412c5adbf95064a2657bb2b850c6b29291
Miroslav Buček <miroslavbucek@gmail.com>
2021-04-05 13:54:57 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -1,13 +1,30 @@
 #!/bin/bash
-
-# update
 apt-get update
 
 # install basic packages
 apt-get -y install vim zsh git mc wget htop avahi-daemon
 
+# add ssh key
+authorizedkeys=~/.ssh/authorized_keys
+sshkey="ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILl4iWGpgvCKPKiJtE0gJ6IIw1ro+20v/o+r4zi96J+u miroslavbucek@gmail.com"
+mkdir -p ~/.ssh/
+touch $authorizedkeys
+grep -qxF "$sshkey" $authorizedkeys || echo "$sshkey" >> $authorizedkeys
+
+# secure ssh
+sshfile="/etc/ssh/sshd_config"
+declare -A rules=(
+    ["UsePAM"]="no"
+    ["PasswordAuthentication"]="no"
+)
+for rule in "${!rules[@]}"; do
+awk -v key="${rule}" -v val="${rules[${rule}]}" \
+  '$1==key {foundLine=1; print key " " val} $1!=key{print $0} END{if(foundLine!=1) print key " " val}' \
+  $sshfile > sshd_config.tmp && mv sshd_config.tmp $sshfile
+done
+
 # timezone Prague
-echo "Europe/Prague" > /etc/timezone
+echo "Europe/Prague" > /etc/timezone 
 dpkg-reconfigure -f noninteractive tzdata
 
 # security updates
@@ -19,12 +36,11 @@ sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install
 
 # install czech
 locale-gen cs_CZ.UTF-8
-echo '
+echo '#
+# cestina
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 # promt s user + hostname
-PROMPT="%n@%m%{$reset_color%} ${PROMPT}"
-# enable completion
-autoload -Uz compinit && compinit' >> ~/.zshrc
+PROMPT="%n@%m%{$reset_color%} ${PROMPT}"' >> ~/.zshrc
