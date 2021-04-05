76b4b59880588a5e39f7282390910dc743ed4660
Miroslav Buček <miroslavbucek@gmail.com>
2021-04-05 14:08:08 +0200

    Update basic.sh

--- basic.sh
+++ basic.sh
@@ -12,16 +12,18 @@ touch $authorizedkeys
 grep -qxF "$sshkey" $authorizedkeys || echo "$sshkey" >> $authorizedkeys
 
 # secure ssh
-sshfile="/etc/ssh/sshd_config"
-declare -A rules=(
-    ["UsePAM"]="no"
-    ["PasswordAuthentication"]="no"
-)
-for rule in "${!rules[@]}"; do
-awk -v key="${rule}" -v val="${rules[${rule}]}" \
-  '$1==key {foundLine=1; print key " " val} $1!=key{print $0} END{if(foundLine!=1) print key " " val}' \
-  $sshfile > sshd_config.tmp && mv sshd_config.tmp $sshfile
-done
+if [ "root" == "$USER" ]; then
+  sshfile="/etc/ssh/sshd_config"
+  declare -A rules=(
+      ["UsePAM"]="no"
+      ["PasswordAuthentication"]="no"
+  )
+  for rule in "${!rules[@]}"; do
+  awk -v key="${rule}" -v val="${rules[${rule}]}" \
+    '$1==key {foundLine=1; print key " " val} $1!=key{print $0} END{if(foundLine!=1) print key " " val}' \
+    $sshfile > sshd_config.tmp && mv sshd_config.tmp $sshfile
+  done
+fi
 
 # timezone Prague
 echo "Europe/Prague" > /etc/timezone 
@@ -35,6 +37,7 @@ apt-get -y install unattended-upgrades
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
 # install czech
+if ! grep -q "@mbb" ~/.zshrc; then
 locale-gen cs_CZ.UTF-8
 echo '# @mbb
 # cestina
@@ -47,3 +50,4 @@ PROMPT="%n@%m%{$reset_color%} ${PROMPT}"
 
 # enable completion
 autoload -Uz compinit && compinit' >> ~/.zshrc
+fi
