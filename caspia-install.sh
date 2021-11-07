64de32ccc24f0140174851e58dd3c3d3a9fffc1e
Miroslav Buček <miroslavbucek@gmail.com>
2021-11-07 15:37:42 +0100

    Update caspia-install.sh

--- caspia-install.sh
+++ caspia-install.sh
@@ -24,7 +24,7 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli
 
-sudo gpasswd -a $USER docker
+sudo gpasswd -a caspia docker
 
 # nastavení docker registry
 docker login -u $USER -p $PASS registry.caspiatech.cz
