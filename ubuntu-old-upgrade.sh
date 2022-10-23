41e7ae4cbaa78b05dbbcace6c6caebfe1f834413
Miroslav Buček <miroslavbucek@gmail.com>
2022-10-23 13:38:43 +0200

    Update ubuntu-old-upgrade.sh

--- ubuntu-old-upgrade.sh
+++ ubuntu-old-upgrade.sh
@@ -3,7 +3,8 @@
 sudo sed -i -re 's/([a-z]{2}\.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 sudo apt update
 sudo apt upgrade
+sudo apt dist-upgrade
 
 echo "------------------------"
-echo "ted restart a potom"
+echo "ted reboot a potom"
 echo "sudo do-release-upgrade"
