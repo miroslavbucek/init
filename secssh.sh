2eb28bb38fae8b2c1b2f42b18f6c288df00f6956
Miroslav Buček <miroslavbucek@gmail.com>
2023-10-09 19:46:45 +0200

    Update secssh.sh

--- secssh.sh
+++ secssh.sh
@@ -5,8 +5,7 @@ if [ "root" == "$USER" ]; then
   sshfile="/etc/ssh/sshd_config"
   declare -A rules=(
       ["UsePAM"]="no"
-      ["PasswordAuthentication"]="no"
-  )
+      ["PasswordAuthentication"]="no")
   for rule in "${!rules[@]}"; do
   awk -v key="${rule}" -v val="${rules[${rule}]}" \
     '$1==key {foundLine=1; print key " " val} $1!=key{print $0} END{if(foundLine!=1) print key " " val}' \
